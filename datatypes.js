7b6c486ce3c2d2e9cf7eb102d8919d620cccb4d4
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2017-01-31 15:45:23 -0200

    Adding datatypes

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+var myString = undefined;
+var myNumber = undefined;
+var myBoolean = undefined;
+
+console.log("Name:" + myString);
+console.log("Lucky number:" + myNumber);
+console.log("Good joke? " + myBoolean)'
