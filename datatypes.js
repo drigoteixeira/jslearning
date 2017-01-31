68830e91b50763e978e47aa33cfee9a848a8427e
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2017-01-31 15:48:41 -0200

    Adding datatypes

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,12 @@
+// DATA TYPES
+
+// STRING: Any grouping of words or numbers surrounded by single quotes
 var myString = undefined;
+
+// Number: Any number, including numbers with decimals, without quotes
 var myNumber = undefined;
+
+// Boolean: This is always one of two words. Either true or false, with no quotations.
 var myBoolean = undefined;
 
 console.log("Name:" + myString);
